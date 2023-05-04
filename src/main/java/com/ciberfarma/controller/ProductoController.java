d8941a98a0d00252ed54160b0d8c0009f524e9b6
PaulPerea <i202013664@cibertec.edu.pe>
2023-05-03 22:06:37 -0500

    Todo listo

--- src/main/java/com/ciberfarma/controller/ProductoController.java
+++ src/main/java/com/ciberfarma/controller/ProductoController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.ciberfarma.repository.ICategoriaRepository;
+import com.ciberfarma.repository.IProductoRepository;
 import com.ciberfarma.repository.IProveedorRepository;
 
 @Controller
@@ -14,6 +15,8 @@ public class ProductoController {
 	private ICategoriaRepository repoCat;
 	@Autowired
 	private IProveedorRepository repoProv;
+	@Autowired
+	private IProductoRepository repoProd;
 	
 	
 	//crea los controladores
@@ -27,6 +30,16 @@ public class ProductoController {
 		//model.addAttribute("mensaje","Exito");
 		model.addAttribute("lstCategorias",repoCat.findAll());
 		model.addAttribute("lstProveedor",repoProv.findAll());
+		return "crudproductos";
+	}
+	@GetMapping("/listado")
+	public String muestraListado(Model model) {
+		
+		//crear obj de repository
+		//enviar un listado para el combo
+		//model.addAttribute("mensaje","Exito");
+		model.addAttribute("lstCategorias",repoCat.findAll());
+		model.addAttribute("lstProdutos",repoProd.findAll());
 		return "crudproductos";
 	}
 	
--- src/main/java/com/ciberfarma/repository/IProveedorRepository.java
+++ src/main/java/com/ciberfarma/repository/IProveedorRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ciberfarma.model.Proveedores;
 
-public interface IProveedorRepository extends JpaRepository<Proveedores, Integer>{
+public interface IProveedorRepository extends JpaRepository<Proveedores, String>{
 
 }
